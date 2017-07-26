17838f55ead340321e906bb56a51e8f68369f7a9
vagrant <vagrant@homestead>
2017-07-26 16:41:36 +0000

    add relationship to models

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'description', 'image_url'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'user_id'
+    ];
+    
+    /**
+     * A category may belong to another category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function parent() {
+        return $this->belongsTo(Category::class);
+    }
+    
+    /*
+     * Product belongs to a user
+     */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'description', 'price', 'image_url', 'options'
+    ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'user_id'
+    ];
+
+    /*
+     * Product belongs to a category
+     */
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    /*
+     * Product belongs to a user
+     */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
